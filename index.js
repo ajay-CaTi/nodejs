cd691b19b73316d2eb7f44fdd9bdea878484bdb1
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-09-10 14:36:20 +0530

    synchronous module

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const lib = require("./lib.js");
 const fs = require("fs");
 
+const text = fs.readFileSync("demo.txt", "utf-8");
+console.log(text);
+
+// this is synchronous, & to read a file it needs some time
+// inbetween this time our server is not blocked so make it asynchronous
+
 console.log(lib.sum(4, 5), lib.diff(7, 9));
